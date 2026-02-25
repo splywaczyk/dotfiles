9db36f3ea24a6dda600f0299c5491120e739e026
Seweryn Plywaczyk <seweryn.plywaczyk3@globallogic.com>
2026-02-25 23:31:57 +0100

    Fix lazygit installation

--- setup.sh
+++ setup.sh
@@ -24,11 +24,10 @@ nvm install node
 npm install -g @google/gemini-cli
 
 # Install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf /tmp/lazygit.tar.gz -C /tmp lazygit
-sudo install /tmp/lazygit /usr/local/bin
-rm /tmp/lazygit /tmp/lazygit.tar.gz
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # Install Oh My Zsh
 echo "=== Installing Oh My Zsh ==="
